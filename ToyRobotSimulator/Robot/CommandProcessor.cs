d67009ecc480992e78be0751d6dc791259cf6443
VinithPhilipose <69187820+VinithPhilipose@users.noreply.github.com>
2025-08-19 20:04:28 +0530

    For Error Handling

--- ToyRobotSimulator/Robot/CommandProcessor.cs
+++ ToyRobotSimulator/Robot/CommandProcessor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -16,34 +16,56 @@ namespace ToyRobotSimulator.Robot
 
         public string Execute(string line)
         {
-            if (string.IsNullOrWhiteSpace(line)) return null;
-            var tokens = line.Trim().Split(' ', 2);
-            var cmd = tokens[0].ToUpperInvariant();
-
-            switch (cmd)
+            try
             {
-                case "PLACE": return HandlePlace(tokens.Length > 1 ? tokens[1] : null);
-                case "MOVE": _robot.Move(); break;
-                case "LEFT": _robot.Left(); break;
-                case "RIGHT": _robot.Right(); break;
-                case "REPORT":
-                    var r = _robot.Report();
-                    Console.WriteLine("Output:"+ r);
-                    return r;
+                if (string.IsNullOrWhiteSpace(line))
+                    throw new ArgumentException("Input line cannot be empty.");
+                var tokens = line.Trim().Split(' ', 2);
+                var cmd = tokens[0].ToUpperInvariant();
+
+                switch (cmd)
+                {
+                    case "PLACE": return HandlePlace(tokens.Length > 1 ? tokens[1] : string.Empty);
+                    case "MOVE": _robot.Move(); break;
+                    case "LEFT": _robot.Left(); break;
+                    case "RIGHT": _robot.Right(); break;
+                    case "REPORT":
+                        var r = _robot.Report();
+                        Console.WriteLine("Output:" + r);
+                        return r;
+                }
+                return String.Empty;
             }
-            return null;
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            return String.Empty;
         }
 
         private string HandlePlace(string args)
         {
-            if (string.IsNullOrWhiteSpace(args)) return null;
-            var parts = args.Split(',');
-            if (parts.Length != 3) return null;
-            if (!int.TryParse(parts[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out var x)) return null;
-            if (!int.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out var y)) return null;
-            if (!DirectionExtensions.TryParse(parts[2], out var dir)) return null;
+            try
+            {
+                if (string.IsNullOrEmpty(args))
+                    throw new ArgumentException("Place command cannot be empty.");
+                var parts = args.Split(',');
+                if (parts.Length != 3)
+                    throw new ArgumentException("Incorrect part length.");
+                if (!int.TryParse(parts[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out var x)) return String.Empty;
+                if (!int.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out var y)) return String.Empty;
+                if (!DirectionExtensions.TryParse(parts[2], out var dir))
+                    throw new ArgumentException("Invalid Direction");
 
-            return _robot.Place(new Position(x, y), dir) ? "PLACED" : null;
+                return _robot.Place(new Position(x, y), dir) ? "PLACED" : "Not PLACED";
+            }
+
+            catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+                return String.Empty;
+            }
+            
         }
     }
     
